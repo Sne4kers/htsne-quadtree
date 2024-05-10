Fixed center of mass calculation

code.py
@@ -4,10 +4,13 @@ import tree
 import numpy as np
 
 # Random testing
+experiments = 1000
 
+a = 0
+b = 0
 
 # generate points
-n = 500
+n = 100
 
 points = []
 for i in range(n):
@@ -17,6 +20,11 @@ for i in range(n):
         x = np.random.uniform(-1, 1)
         y = np.random.uniform(-1, 1)
     points.append((x, y))
+
+    # Add radially pposite node
+    points.append((-x, -y))
+
+print("POINTS ", points)
 
 # make the tree
 my_tree = tree.PyInfinityQuadTree(points)
@@ -38,7 +46,6 @@ fig.set_figwidth(15)
 pdisk = patches.Circle((0, 0), 1, linewidth=1, edgecolor='b', facecolor='none')
 ax.add_patch(pdisk)
 
-
 for p in points:
     ax.scatter(p[0], p[1], color="g", s=5)
 
@@ -49,9 +56,13 @@ for i in my_tree.get_nodes():
         color = "r"
     else:
         color = "b"
-        if i["depth"] == selected_depth:
-            ax.scatter(i["barycenter"]["x"], i["barycenter"]["y"], c="orange", alpha=0.5)
+        if i["depth"] == 0:
+            print(i["barycenter"]["x"], i["barycenter"]["y"])
+    ax.scatter(i["barycenter"]["x"], i["barycenter"]["y"], c="orange", alpha=0.5)
     rect = patches.Rectangle((i["min_bounds"]["x"], i["min_bounds"]["y"]), i["max_bounds"]["x"] - i["min_bounds"]["x"], i["max_bounds"]["y"] - i["min_bounds"]["y"], linewidth=0.25, edgecolor=color, facecolor='none')
     ax.add_patch(rect)
 
 plt.savefig('foo.png')
+# a /= experiments
+# b /= experiments
+# print("FINAL RESULTS ", a, b)